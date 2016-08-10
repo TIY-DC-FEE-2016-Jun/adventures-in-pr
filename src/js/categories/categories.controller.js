e4bf824ed614e7a791243004f1317a5a88d88695
Samina Chhatwal <csaminak@gmail.com>
2016-08-10 16:56:29 -0400

    removed retrieve categories function details and moved into create post

--- src/js/categories/categories.controller.js
+++ src/js/categories/categories.controller.js
@@ -6,19 +6,7 @@
 
     CategoriesController.$inject = ['blogsite'];
 
-    function CategoriesController(blogsite) {
-        var that = this;
-        this.categories = [];
-
-        blogsite.getAllCategories()
-            .then(function(data) {
-                that.categories = data;
-            })
-            .catch(function(error) {
-                that.categories = [{name: 'no category list exists'}];
-                console.error(error);
-            });
-
+    function CategoriesController() {
 
     }
 
