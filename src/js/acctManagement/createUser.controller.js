19ee56466ec3cb9e0800dbceee35738474538ebe
Stella Ma <sm873x@gmail.com>
2016-08-10 16:50:04 -0400

    Moved createUser and login controllers into acctManagement folder, the correct folder.

---
+++ src/js/acctManagement/createUser.controller.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular.module('blog')
+        .controller('CreateUserController', CreateUserController);
+
+    function CreateUserController() {
+
+    }
+
+})();
