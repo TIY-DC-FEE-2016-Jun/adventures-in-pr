240a6a621bdb918fd7adc86b8a51c76cf5e50814
Samina Chhatwal <csaminak@gmail.com>
2016-08-10 16:22:29 -0400

    updated categories controller to save the data retrieved from service

--- src/js/categories/categories.controller.js
+++ src/js/categories/categories.controller.js
@@ -7,7 +7,19 @@
     CategoriesController.$inject = ['blogsite'];
 
     function CategoriesController(blogsite) {
-        this.categories = blogsite.getAllCategories;
+        var that = this;
+        this.categories = [];
+
+        blogsite.getAllCategories()
+            .then(function(data) {
+                that.categories = data;
+            })
+            .catch(function(error) {
+                that.categories = [{name: 'no category list exists'}];
+                console.error(error);
+            });
+
+
     }
 
 
