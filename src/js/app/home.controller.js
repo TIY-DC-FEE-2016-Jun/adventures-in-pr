3a6e289ddb95ee4164fee3c2164b4b8be97561ed
Samina Chhatwal <csaminak@gmail.com>
2016-08-10 17:00:14 -0400

    created home controller to added categories into aside

---
+++ src/js/app/home.controller.js
@@ -0,0 +1,25 @@
+(function(){
+    'use strict';
+
+    angular.module('blog')
+        .controller('HomeController', HomeController);
+
+    CreatePostController.$inject = ['blogsite'];
+
+    function HomeController(blogsite) {
+        var that = this;
+        this.categories = [];
+
+        blogsite.getAllCategories()
+            .then(function(data) {
+                that.categories = data;
+            })
+            .catch(function(error) {
+                that.categories = [{name: 'no category list exists'}];
+                console.error(error);
+            });
+
+
+    }
+
+})();
