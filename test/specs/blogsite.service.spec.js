8c42c37c084fa967119593b53ebaa4911b78c5cd
Samina Chhatwal <csaminak@gmail.com>
2016-08-11 16:52:29 -0400

    added assert description

--- test/specs/blogsite.service.spec.js
+++ test/specs/blogsite.service.spec.js
@@ -12,16 +12,16 @@
             blogsite = _blogsite_;
             $httpBackend = _$httpBackend_;
 
-        
+
 
         }));
 
         test('blogsite service functions exist', function() {
-            assert.isFunction(blogsite.createUser, ' function exists');
-            assert.isFunction(blogsite.getAllCategories, ' function exists');
-            assert.isFunction(blogsite.login, ' function exists');
-            assert.isFunction(blogsite.isLoggedIn, ' function exists');
-            assert.isFunction(blogsite.logOut, ' function exists');
+            assert.isFunction(blogsite.createUser, 'createUser function exists');
+            assert.isFunction(blogsite.getAllCategories, 'getAllCategories function exists');
+            assert.isFunction(blogsite.login, 'login function exists');
+            assert.isFunction(blogsite.isLoggedIn, 'isLoggedIn function exists');
+            assert.isFunction(blogsite.logOut, 'logOut function exists');
         });
 
     });
