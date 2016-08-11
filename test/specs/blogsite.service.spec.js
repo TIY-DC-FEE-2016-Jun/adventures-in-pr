764174506ba9ac999fe93b90fea9b61884c0b775
Samina Chhatwal <csaminak@gmail.com>
2016-08-11 14:52:13 -0400

    started created sanity test for functions/mehtods in service file

--- test/specs/blogsite.service.spec.js
+++ test/specs/blogsite.service.spec.js
@@ -3,7 +3,26 @@
 
     var assert = chai.assert;
 
-    suite('');
+    suite('blogsite service', function() {
+        var blogsite, $httpBackend;
+
+        setup(module('blog'));
+
+        setup(inject(function(_blogsite_, _$httpBackend_) {
+            blogsite = _blogsite_;
+            $httpBackend = _$httpBackend_;
+
+        }));
+
+        test('blogsite service functions exist', function() {
+            assert.isFunction(blogsite.createUser, ' function exists');
+            assert.isFunction(blogsite.getAllCategories, ' function exists');
+            assert.isFunction(blogsite.login, ' function exists');
+            assert.isFunction(blogsite.isLoggedIn, ' function exists');
+            assert.isFunction(blogsite.logOut, ' function exists');
+        });
+
+    });
 
 
 
