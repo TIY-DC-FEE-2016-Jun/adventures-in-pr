1eb392db4200609307841c73c05e65ce8e9ef7c4
Stella Ma <sm873x@gmail.com>
2016-08-12 14:57:58 -0400

    Added test for successful creation of new author. Added test to ensure that new author requires a name.

--- test/specs/blogsite.service.spec.js
+++ test/specs/blogsite.service.spec.js
@@ -22,6 +22,26 @@
                 .whenGET('/js/app/home.template.html')
                 .respond('<h1>Mock Home Template</h1>');
 
+            $httpBackend
+                .when('POST', 'https://tiy-blog-api.herokuapp.com/api/Authors', {
+                    name: 'Jon',
+                    email: 'jon@yahoo.com',
+                    password: '123456789'
+                })
+                .respond({
+                    data: {email: 'jon@yahoo.com', id: '1234', name: 'Jon'}
+                });
+
+            $httpBackend
+                .when('POST', 'https://tiy-blog-api.herokuapp.com/api/Authors', {
+                    name: null,
+                    email: 'jon@yahoo.com',
+                    password: '123456789'
+                })
+                .respond({
+                    err: {status: 401}
+                });
+
         }));
 
         test('blogsite service functions exist', function() {
@@ -30,6 +50,51 @@
             assert.isFunction(blogsite.login, 'login function exists');
             assert.isFunction(blogsite.isLoggedIn, 'isLoggedIn function exists');
             assert.isFunction(blogsite.logOut, 'logOut function exists');
+        });
+
+        test('create user fn successfully returns new author object', function(done) {
+            var result = blogsite.createUser('Jon', 'jon@yahoo.com', '123456789');
+
+            assert.isObject(result, 'createUser returns an object');
+            assert.isFunction(result.then, 'result has a then method');
+            assert.isFunction(result.catch, 'result has a catch method');
+
+            result
+                .then(function(newAuthor) {
+                    assert
+                        .strictEqual(
+                            newAuthor.data.data.email,
+                            'jon@yahoo.com',
+                            'newAuthor has an email'
+                        );
+                    assert.strictEqual(newAuthor.data.data.name, 'Jon', 'newAuthor has a name');
+                    assert.strictEqual(newAuthor.data.data.id, '1234', 'newAuthor has an id');
+                    done();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    assert.isFail('should not be in catch for createAuthor');
+                    done();
+                });
+
+            $httpBackend.flush();
+        });
+
+        test('cannot create a new author without a name', function(done) {
+            var result = blogsite.createUser(null, 'jon@yahoo.com', '123456789');
+
+            result
+                .then(function(err) {
+                    console.log(err);
+                    assert.fail('should not be in then if author has no name');
+                    done();
+                })
+                .catch(function(err) {
+                    assert.strictEqual(err.status, '401', 'cannot create new author w/o name');
+                    done();
+                });
+
+            $httpBackend.flush();
         });
 
         test('getAllCategories able to retrieve categories', function(done) {
