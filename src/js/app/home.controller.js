d0705cc87e8356ca5063ff78cb7736d993cc44fc
Samina Chhatwal <csaminak@gmail.com>
2016-08-12 21:31:58 -0400

    created home controller for the home view

---
+++ src/js/app/home.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular.module('blog')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['blogsite'];
+
+    function HomeController(blogsite) {
+
+    }
+
+
+})();
