4e60d44cc9dc00ec4a06cd1cc58d87fd4e63158c
Stella Ma <sm873x@gmail.com>
2016-08-15 00:50:45 -0400

    Added monthlyBlog empty array and getMonthlyBlogs function. GetMonthlyBlogs is supposed to loop through the array return by getAllBlogs from the service.

--- src/js/posts/recentPosts.controller.js
+++ src/js/posts/recentPosts.controller.js
@@ -4,15 +4,33 @@
     angular.module('blog')
         .controller('RecentPostsController', RecentPostsController);
 
-    RecentPostsController.$inject = ['$stateParams'];
+    RecentPostsController.$inject = ['$stateParams', 'blogsite'];
 
-    function RecentPostsController($stateParams) {
+    function RecentPostsController($stateParams, blogsite) {
+        var that = this;
+
         this.monthName = $stateParams.chosenMonth;
         this.allDates = JSON.parse($stateParams.allDates);
         this.dateIndex = $stateParams.dateIndex;
 
         this.selMonth = new Date(this.allDates[this.dateIndex]).getMonth();
 
+        this.monthlyBlogs = [];
+        this.getMonthlyBlogs = function getMonthlyBlogs() {
+            blogsite.getAllBlogs()
+                .then(function(blogs) {
+                    console.log('all blogs', blogs);
+
+                    for (var i=0; i<(blogs.data.length - 1); i++) {
+                        var month = new Date(blogs[i].date).getMonth();
+                        if ( month === that.selMonth ) {
+                            that.monthlyBlogs.push(blogs[i]);
+                        }
+                    }
+                    console.log(that.monthBlogs);
+                });
+
+        };
 
     }
 })();
