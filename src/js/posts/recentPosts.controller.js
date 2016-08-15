b5db8bb2f208a50b93a31cc874110fcf0b09067b
Samina Chhatwal <csaminak@gmail.com>
2016-08-15 14:36:44 -0400

    commented out month feature that we can't support

--- src/js/posts/recentPosts.controller.js
+++ src/js/posts/recentPosts.controller.js
@@ -1,14 +1,14 @@
-(function() {
-    'use strict';
-
-    // angular.module('blog')
-    //     .controller('RecentPostsController', RecentPostsController);
-    //
-    // RecentPostsController.$inject = ['blogsite'];
-    //
-    // function RecentPostsController() {
-    //     var that = this;
-    //
-    //
-    // }
-})();
+// (function() {
+//     'use strict';
+//
+//     // angular.module('blog')
+//     //     .controller('RecentPostsController', RecentPostsController);
+//     //
+//     // RecentPostsController.$inject = ['blogsite'];
+//     //
+//     // function RecentPostsController() {
+//     //     var that = this;
+//     //
+//     //
+//     // }
+// })();
