87cab974f4fe84fb70ebb55868dde029f01bcdab
Stella Ma <sm873x@gmail.com>
2016-08-14 23:31:47 -0400

    Added doc block for getPastThreeMonths fn within service

--- src/js/app/blogsite.service.js
+++ src/js/app/blogsite.service.js
@@ -364,6 +364,13 @@
             });
         }
 
+        /**
+         * Return array of date objects from the past three months
+         * starting with the current month
+         * @param  {Object} currentDate Date object for current date
+         * @return {Array}             Array of date objects for the
+         *                             past three months
+         */
         function getPastThreeMonths(currentDate) {
             var dateObjects = [];
             var oneMonth = 2629746000;
