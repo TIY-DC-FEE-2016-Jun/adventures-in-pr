944827259c4eeed8c193751be79851b01d94cdd4
Samina Chhatwal <csaminak@gmail.com>
2016-08-14 21:01:04 -0400

    "accepted merge changes"

--- src/js/app/home.controller.js
+++ src/js/app/home.controller.js
@@ -10,37 +10,16 @@
         var that = this;
 
         this.allBlogs = [];
-
-<<<<<<< HEAD
-        this.goToPost = function goToPost(author, title, id) {
-            $state.go('post', {'authorName': author, 'postTitle': title, 'postId': id});
-        };
-=======
-        /**
-         * Assigns first sentence of blog post to content property of blog data
-         * @param  {Object} blog Object with the blog post data
-         * @return {void}
-         */
-        function getFirstSentence(blog) {
-            blog.content = blog.content.substring(0, blog.content.indexOf('.')+1) ;
-        }
->>>>>>> upstream/master
-
-        blogsite.getAllBlogs()
-            .then(function(blogs) {
-                blogs.forEach(function(blog) {
-                    getFirstSentence(blog);
-                });
-                that.allBlogs = blogs;
-            });
-
         this.limit = 5;
         this.offset = 0;
         this.offsetIncrement = 5;
 
+        this.goToPost = function goToPost(author, title, id) {
+            $state.go('post', {'authorName': author, 'postTitle': title, 'postId': id});
+        };
+
         this.goToOlderPosts = function goToOlderPosts() {
             that.offset = (that.offset + that.offsetIncrement);
-
             blogsite.getPostByDate(that.limit, that.offset)
                 .then(function(olderBlogs) {
                     var olderBlogsArray = olderBlogs.data;
@@ -48,11 +27,9 @@
                     if (olderBlogsArray === []) {
                         return null;
                     }
-
                     olderBlogsArray.forEach(function(blog) {
                         getFirstSentence(blog);
                     });
-
                     that.allBlogs = olderBlogsArray;
                 })
                 .catch(function(err) {
@@ -62,20 +39,16 @@
 
         this.goToNewerPosts = function goToNewerPosts() {
             that.offset = (that.offset - that.offsetIncrement);
-
             if (that.offset < 0) {
                 that.offset = 0;
             }
-
             blogsite.getPostByDate(that.limit, that.offset)
                 .then(function(newerBlogs) {
                     var newerBlogsArray = newerBlogs.data;
                     console.log('newerBlogs', newerBlogsArray);
-
                     newerBlogsArray.forEach(function(blog) {
                         getFirstSentence(blog);
                     });
-
                     that.allBlogs = newerBlogsArray;
                 })
                 .catch(function(err) {
@@ -86,6 +59,25 @@
         this.goToPost = function goToPost(author, title, id) {
             $state.go('post', {'authorName': author, 'postTitle': title, 'postId': id});
         };
+
+
+        /**
+         * Assigns first sentence of blog post to content property of blog data
+         * @param  {Object} blog Object with the blog post data
+         * @return {void}
+         */
+        function getFirstSentence(blog) {
+            blog.content = blog.content.substring(0, blog.content.indexOf('.')+1) ;
+        }
+
+        blogsite.getAllBlogs()
+            .then(function(blogs) {
+                blogs.forEach(function(blog) {
+                    getFirstSentence(blog);
+                });
+                that.allBlogs = blogs;
+            });
+
     }
 
 
