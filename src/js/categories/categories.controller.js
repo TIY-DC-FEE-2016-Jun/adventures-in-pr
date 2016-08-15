af052169b50e7f9bf0d69b6357b20b16bb2cdab6
Samina Chhatwal <csaminak@gmail.com>
2016-08-14 21:58:15 -0400

    delete functionality is working

--- src/js/categories/categories.controller.js
+++ src/js/categories/categories.controller.js
@@ -4,24 +4,43 @@
     angular.module('blog')
         .controller('CategoriesController', CategoriesController);
 
-    CategoriesController.$inject = ['$stateParams', 'blogsite'];
+    CategoriesController.$inject = ['$stateParams', '$state', 'blogsite'];
 
-    function CategoriesController($stateParams, blogsite) {
+    function CategoriesController($stateParams, $state, blogsite) {
         var that = this;
         this.allBlogs = [];
         this.categoryId = $stateParams.categoryId;
         this.categoryName = null;
         this.delete = deletePost;
 
-        blogsite.getCategory(this.categoryId)
-            .then(function(blogs) {
-                console.log('blog in getPostsInCategory', blogs.posts);
-                that.allBlogs = blogs.posts;
-                that.categoryName = blogs.name;
-            });
+        categoryInit();
 
-        function deletePost() {
-            
+        /**
+         * Loads the info needed for the category view
+         * @return {Void}
+         */
+        function categoryInit() {
+            blogsite.getCategory(that.categoryId)
+                .then(function(blogs) {
+                    that.allBlogs = blogs.posts;
+                    that.categoryName = blogs.name;
+                });
+        }
+
+        /**
+         * Deletes a post with given blog Id
+         * @param  {String}   blogId    id of blog to delete
+         * @return {Void}
+         */
+        function deletePost(blogId) {
+            console.log('in deletePost', blogId);
+            blogsite.deleteBlogPost(blogId);
+            that.allBlogs.forEach(function(blog) {
+                if (blogId === blog.id) {
+                    that.allBlogs.splice(that.allBlogs.indexOf(blog), 1);
+                }
+            });
+            return that.allBlogs;
         }
     }
 
