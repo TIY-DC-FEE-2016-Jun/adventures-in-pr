2299ca53bcaf0a4fcabdaceb5dd75a2569f6b5e3
Stella Ma <sm873x@gmail.com>
2016-08-14 20:40:13 -0400

    Added goToOlderPosts function in controller that calls the getPostByDate fn in service to show the previous five posts.

--- src/js/app/home.controller.js
+++ src/js/app/home.controller.js
@@ -8,20 +8,59 @@
 
     function HomeController($state, blogsite) {
         var that = this;
+
         this.allBlogs = [];
-        this.goToPost = function goToPost(author, title, id) {
-            $state.go('post', {'authorName': author, 'postTitle': title, 'postId': id});
-        };
+
+        /**
+         * Assigns first sentence of blog post to content property of blog data
+         * @param  {Object} blog Object with the blog post data
+         * @return {void}
+         */
+        function getFirstSentence(blog) {
+            blog.content = blog.content.substring(0, blog.content.indexOf('.')+1) ;
+        }
 
         blogsite.getAllBlogs()
             .then(function(blogs) {
                 console.log('blogs returned from getAllBlogs:', blogs);
                 blogs.forEach(function(blog) {
-                    blog.content = blog.content.substring(0, blog.content.indexOf('.')+1) ;
+                    getFirstSentence(blog);
                 });
                 that.allBlogs = blogs;
             });
 
+        this.limit = 5;
+        this.offset = 0;
+        this.offsetIncrement = 5;
+        
+        this.goToOlderPosts = function goToOlderPosts() {
+            that.offset = (that.offset + that.offsetIncrement);
+
+            blogsite.getPostByDate(that.limit, that.offset)
+                .then(function(olderBlogs) {
+                    var olderBlogsArray = olderBlogs.data;
+                    console.log('olderBlogs', olderBlogsArray);
+                    if (olderBlogsArray === []) {
+                        return null;
+                    }
+
+                    olderBlogsArray.forEach(function(blog) {
+                        getFirstSentence(blog);
+                    });
+
+                    that.allBlogs = olderBlogsArray;
+                })
+                .catch(function(err) {
+                    console.error('unable to get older posts', err.status);
+                });
+
+            console.log('new offset', that.offset);
+        };
+
+
+        this.goToPost = function goToPost(author, title, id) {
+            $state.go('post', {'authorName': author, 'postTitle': title, 'postId': id});
+        };
     }
 
 
